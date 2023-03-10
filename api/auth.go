db83b81b189d6c323b1ca07533b242091ef569a4
Daniil Tsivinsky <daniil@tsivinsky.com>
2023-03-10 15:16:30 +0300

    feat(auth): add middleware for checking user authentication

--- api/auth.go
+++ api/auth.go
@@ -1,6 +1,12 @@
 package main
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"errors"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func HashPassword(password string) (string, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
@@ -12,4 +18,16 @@ func CheckPassword(hash string, password string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 
 	return err == nil
+}
+
+func GetAccessTokenFromHeader(c *fiber.Ctx) (string, error) {
+	authHeader := c.Get("Authorization")
+
+	if authHeader == "" {
+		return "", errors.New("No Authorization header in request")
+	}
+
+	accessToken := strings.Split(authHeader, " ")[1]
+
+	return accessToken, nil
 }
