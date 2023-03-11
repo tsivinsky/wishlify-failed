77b9e1b0da555edb59b23fc0d371965e7585acaa
Daniil Tsivinsky <daniil@tsivinsky.com>
2023-03-11 10:37:53 +0300

    feat(products): remove `Wishlist` from product model

--- api/product.go
+++ api/product.go
@@ -3,8 +3,7 @@ package main
 type Product struct {
 	Model
 
-	Title      string   `json:"title" gorm:"title"`
-	Image      *string  `json:"image" gorm:"image"`
-	WishlistId uint     `json:"wishlistId" gorm:"wishlist_id"`
-	Wishlist   Wishlist `json:"wishlist" gorm:"wishlist"`
+	Title      string  `json:"title" gorm:"title"`
+	Image      *string `json:"image" gorm:"image"`
+	WishlistId uint    `json:"wishlistId" gorm:"wishlist_id"`
 }
